15a66f469d6a0b34ddb191f5ac92bb98010f08f9
unknown <tara.zver22@gmail.com>
2023-01-11 09:25:22 +0100

    Added listing and migrations.

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Listing(models.Model): #inheritance -> class interpreted as a DB table
+    # add columns in a table
+    title = models.CharField(max_length=150)
+    price = models.IntegerField() #or Decimal/Float field
+    num_bedrooms = models.IntegerField()
+    num_bathrooms = models.IntegerField()
+    square_footage = models.IntegerField()
+    address = models.CharField(max_length=100)
+    # image -> more complicated
+
+
